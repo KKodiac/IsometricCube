0c09eb5ac63aef755f4501b70f430f59759e4464
홍성민 <seanhong2000@gmail.com>
2023-06-04 17:33:33 +0900

    Initial Commit

---
+++ IsometricAnimation/IsometricAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  IsometricAnimationApp.swift
+//  IsometricAnimation
+//
+//  Created by Sean Hong on 2023/06/04.
+//
+
+import SwiftUI
+
+@main
+struct IsometricAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
